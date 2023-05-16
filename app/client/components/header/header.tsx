90c3fd0fff476cf411d2e015d398d5d66f9926b2
Nicola Lanzoni <lanzoni.nicola@gmail.com>
2023-05-15 21:37:49 -0300

    feat: update yoga page introduction

--- app/client/components/header/header.tsx
+++ app/client/components/header/header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
             <div className="flex justify-center mb-4">
                 <Logo variant="sky" />
             </div>
-            <div className="flex justify-center gap-8">
+            <div className="flex justify-center gap-6">
                 <HeaderIconGroup variant="instagram" label="Instagram" to="https://www.instagram.com/yogaterapia_dionimara/" />
                 <HeaderIconGroup variant="whatsapp" label="Whatsapp" to="https://wa.me/5546991364002?text=Ol%C3%A1%2C+tudo+bem%3F+Gostaria+agendar+%3Ccoloque+o+servi%C3%A7o%3E" />
                 <HeaderIconGroup variant="directions" label="Rotas" to="https://www.google.com/maps/dir//Dioni%20Mara%20Yoga%20&%20Terapias%20Tv.%20Pinheiro%20Machado,%20362%20-%20La%20Salle,%20Pato%20Branco%20-%20PR,%2085505-060" />
@@ -28,7 +28,7 @@ interface HeaderIconGroupProps {
 
 function HeaderIconGroup({ variant, to, label }: HeaderIconGroupProps) {
 
-    return <div className="flex flex-col justify-content items-center gap-1 hover:bg-violet-100 rounded-md md:p-2">
+    return <div className="flex flex-col justify-content items-center gap-1 hover:bg-violet-100 rounded-md p-2">
         <Icon variant={variant} to={to} />
         <span className="text-xs font-bold text-sky-700">{label}</span>
     </div>
