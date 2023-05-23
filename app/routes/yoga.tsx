b690ede81539f8d9a58a68069b9e9caee32e21e0
Nicola Lanzoni <lanzoni.nicola@gmail.com>
2023-05-23 10:47:41 -0300

    feat: update yoga page

--- app/routes/yoga.tsx
+++ app/routes/yoga.tsx
@@ -8,14 +8,14 @@ import ServiceBox from "~/client/components/service-box/service-box";
 import type { Handle, ServiceUI } from "~/types";
 
 export const handle: Handle = {
-    lastModified: () => '2023-05-18T12:00:00Z',
+    lastModified: () => '2023-05-23T12:00:00Z',
 }
 
 export const meta: V2_MetaFunction = () => {
     return [
-        { title: "Yoga - Dioni Mara Yoga & Terapias" },
-        { description: "Dioni Mara Yoga & Terapias oferece práticas de yoga, terapias, massagem, reiki e meditação para empresas, escolas, academias. Melhore sua flexibilidade, força, equilíbrio e concentração para uma vida saudável e equilibrada. Agende agora pelo WhatsApp." },
-        { keywords: "yoga para crianças, yoga para gestantes, yoga para adulto, meditação, kundalini, yoga online, yoga em grupo, yoga em escolas, yoga em academias" }
+        { title: "Aulas de Yoga em Pato Branco - Dioni Mara Yoga & Terapias" },
+        { description: "Dioni Mara Yoga & Terapias oferece aulas de Yoga em Pato Branco para todos os níveis e idades, além de terapias, massagens, reiki e meditação para empresas, escolas, academias. Melhore sua flexibilidade, força, equilíbrio e concentração para uma vida saudável e equilibrada. Agende agora pelo WhatsApp." },
+        { keywords: "aulas de yoga, aulas de ioga, yoga para crianças, yoga para gestantes, yoga para adulto, meditação, kundalini, yoga online, yoga em grupo, yoga em escolas, yoga em academias" }
     ]
 }
 
@@ -41,8 +41,13 @@ export default function ServicosYoga() {
 
     return <>
         <Container>
-            <h1 className="font-bold text-4xl mb-8">Yoga</h1>
-            <p className="md:text-xl leading-normal">Ofereço uma variedade de estilos de yoga para atender às suas necessidades individuais. Eu tenho opções para todos os níveis de experiência. Com cada tipo de yoga, meu objetivo é promover o bem-estar físico e mental, através da melhoria da flexibilidade, força, equilíbrio e concentração. Além disso, eu sou um instrutor altamente qualificado e estou comprometido em ajudá-lo a desenvolver sua consciência corporal e emocional, visando uma vida mais saudável e equilibrada. Explore meus diferentes tipos de yoga e encontre aquele que melhor se adapta a você!</p>
+            <h1 className="font-bold text-3xl md:text-4xl mb-2">Yoga</h1>
+            <h2 className="font-bold text-lg leading-snug md:text-xl mb-8">Aulas de yoga em Pato Branco para todos os níveis e idades</h2>
+            <div>
+                <p className="md:text-xl leading-normal mb-4">Ofereço uma variedade de estilos de yoga para atender às suas necessidades individuais.</p>
+                <p className="md:text-xl leading-normal mb-4">Eu tenho opções para todos os níveis de experiência. Com cada tipo de yoga, meu objetivo é promover o bem-estar físico e mental, através da melhoria da flexibilidade, força, equilíbrio e concentração.</p>
+                <p className="md:text-xl leading-normal mb-4">Além disso, eu sou uma instrutora altamente qualificada e estou comprometida em ajudar a desenvolver sua consciência corporal e emocional, visando uma vida mais saudável e equilibrada.</p>
+            </div>
         </Container>
         <Container>
             <div className="flex flex-col gap-4 lg:gap-8 mt-8">
